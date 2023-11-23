33ade5d36afba473f3545950f782cb05bf75e6ea
H7nggg <doanngochung2602@gmail.com>
2023-11-23 20:26:49 +0700

    bai2

---
+++ index.js
@@ -0,0 +1,12 @@
+//bai2
+
+function calculateExpression(x, y, z) {
+    return 3 * x**2 - 5 * x * y + z**4;
+}
+
+const x = 5;
+const y = 1;
+const z = 2;
+
+const result = calculateExpression(x, y, z);
+console.log("Result:", result);
